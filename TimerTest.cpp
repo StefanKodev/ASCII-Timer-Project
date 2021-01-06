660020457f5c2e3d9ba32dcaeed07562ada27dc5
StefanKodev <35624303+StefanKodev@users.noreply.github.com>
2021-01-06 21:01:16 +0200

    Finished the project
    Almost finished. Just have to do some edits here and there

--- TimerTest.cpp
+++ TimerTest.cpp
@@ -7,69 +7,157 @@ const int COLS = 11;
 
 const int ROWS = 11;
 
-char canvas[ROWS][COLS * 5] = { {0} };
+const int numOfNums = 10;
+const int numOfLines = 11;
 
-const char CLOCK_CHAR = 'O';
-
-const char* const codes[] =
+const char* const codes[numOfNums][numOfLines] =
 {
-
-	// digit 0 
-	"11111111111000000001100000000110000000011000000001100000000110000000011000000001100000000110000000011111111111",
+	{
+		// digit 0 
+		"0000000000",
+		"0        0",
+		"0        0",
+		"0        0",
+		"0        0",
+		"0        0",
+		"0        0",
+		"0        0",
+		"0        0",
+		"0        0",
+		"0000000000"
+		},
 	// digit 1 
-	"00000000010000000001000000000100000000010000000001000000000100000000010000000001000000000100000000010000000001",
+	{
+	"         1",
+	"         1",
+	"         1",
+	"         1",
+	"         1",
+	"         1",
+	"         1",
+	"         1",
+	"         1",
+	"         1",
+	"         1"
+	},
 	// digit 2 
-	"11111111110000000001000000000100000000010000000001111111111110000000001000000000100000000010000000001111111111",
+	{
+	"2222222222",
+	"         2",
+	"         2",
+	"         2",
+	"         2",
+	"2222222222",
+	"2         ",
+	"2         ",
+	"2         ",
+	"2         ",
+	"2222222222"
+	},
 	// digit 3 
-	"11111111110000000001000000000100000000010000000001111111111100000000010000000001000000000100000000011111111111",
+	{
+	"3333333333",
+	"         3",
+	"         3",
+	"         3",
+	"         3",
+	"3333333333",
+	"         3",
+	"         3",
+	"         3",
+	"         3",
+	"3333333333"
+	},
 	// digit 4 
-	"10000000011000000001100000000110000000011000000001111111111100000000010000000001000000000100000000010000000001",
+		{
+		"4        4",
+		"4        4",
+		"4        4",
+		"4        4",
+		"4        4",
+		"4444444444",
+		"         4",
+		"         4",
+		"         4",
+		"         4",
+		"         4"
+		},
 	// digit 5 
-	"11111111111000000000100000000010000000001000000000111111111100000000010000000001000000000100000000011111111111",
+	{
+	"5555555555",
+	"5         ",
+	"5         ",
+	"5         ",
+	"5         ",
+	"5555555555",
+	"         5",
+	"         5",
+	"         5",
+	"         5",
+	"5555555555"
+	},
 	// digit 6 
-	"11111111111000000000100000000010000000001000000000111111111110000000011000000001100000000110000000011111111111",
+	{
+	"6666666666",
+	"6         ",
+	"6         ",
+	"6         ",
+	"6         ",
+	"6666666666",
+	"6        6",
+	"6        6",
+	"6        6",
+	"6        6",
+	"6666666666"
+	},
 	// digit 7 
-	"11111111110000000001000000000100000000010000000001000000000100000000010000000001000000000100000000010000000001",
+	{
+	"7777777777",
+	"         7",
+	"         7",
+	"         7",
+	"         7",
+	"         7",
+	"         7",
+	"         7",
+	"         7",
+	"         7",
+	"         7"
+	},
 	// digit 8 
-	"11111111111000000001100000000110000000011000000001111111111110000000011000000001100000000110000000011111111111",
+	{
+	"8888888888",
+	"8        8",
+	"8        8",
+	"8        8",
+	"8        8",
+	"8888888888",
+	"8        8",
+	"8        8",
+	"8        8",
+	"8        8",
+	"8888888888"
+	},
 	// digit 9 
-	"11111111111000000001100000000110000000011000000001111111111100000000010000000001000000000100000000011111111111",
-	// semi-colon separator 
-	"00010010000"
+	{
+	"9999999999",
+	"9        9",
+	"9        9",
+	"9        9",
+	"9        9",
+	"9999999999",
+	"         9",
+	"         9",
+	"         9",
+	"         9",
+	"9999999999"
+	}
 };
 
-void storeDigit(int digit, int offset)
-{
-
-	for (int row = 0; row < ROWS; row++)
-	{
-
-		for (int col = 0; col < COLS; col++)
-		{
-
-			int x = offset + col;
-
-			int y = row;
-
-			int dot = COLS * row + col;
-
-			if ('1' == codes[digit][dot])
-			{
-
-				canvas[y][x] = CLOCK_CHAR;
-
-			}
-
-		}
-
-	}
-
-}
-//
 void secToMin(int& secInput, int& minsInput)
 {
-		minsInput++;
-		secInput -= 60;
+	minsInput++;
+	secInput -= 60;
 }
 
 
@@ -84,6 +172,11 @@ int main()
 {
 	int minutes = 0;
 	int seconds = 0;
+	int minHund = 0;
+	int minTens;
+	int minUnits;
+	int secTens;
+	int secUnits;
 	cin >> seconds;
 
 	while (seconds > 60)
@@ -102,34 +195,94 @@ STARTOFTIMER:
 		{
 			minToSec(minutes, seconds);
 		}
-		else cout << "Time is up";
+		else goto ENDOFTIMER;
 	}
 
 	if (minutes < 10)
 	{
-		storeDigit(0, 0);
-		storeDigit(minutes, 11);
-		storeDigit(10, 22);
-		storeDigit(seconds / 10, 24);
-		storeDigit(seconds % 10, 35);
+		minTens = 0;
+		minUnits = minutes % 10;
+		secTens = seconds / 10;
+		secUnits = seconds % 10;
+	}
+	else if (minutes >= 10 && minutes < 100)
+	{
+		minTens = minutes / 10;
+		minUnits = minutes % 10;
+		secTens = seconds / 10;
+		secUnits = seconds % 10;
+	}
+	else
+	{
+		minHund = minutes / 100;
+		minTens = (minutes % 100) / 10;
+		minUnits = minutes % 10;
+		secTens = seconds / 10;
+		secUnits = seconds % 10;
 	}
 
-	//prins canvas
-	for (int row = 0; row < ROWS; row++)
+
+	if (minutes < 100)
 	{
-
-		for (int col = 0; col < COLS * 5; col++)
+		for (int i = 0; i < ROWS; i++)
 		{
-
-			cout << canvas[row][col];
-
+			cout << codes[minTens][i];
+			cout << " ";
+			cout << codes[minUnits][i];
+			cout << " ";
+			if (i == 4 || i == 7) cout << char(178);
+			else cout << " ";
+			cout << " ";
+			cout << codes[secTens][i];
+			cout << " ";
+			cout << codes[secUnits][i];
+			cout << " ";
+			cout << endl;
 		}
-
-		cout << endl;
-
+	}
+	else
+	{
+		for (int i = 0; i < ROWS; i++)
+		{
+			cout << codes[minHund][i];
+			cout << " ";
+			cout << codes[minTens][i];
+			cout << " ";
+			cout << codes[minUnits][i];
+			cout << " ";
+			if (i == 4 || i == 7) cout << char(178);
+			else cout << " ";
+			cout << " ";
+			cout << codes[secTens][i];
+			cout << " ";
+			cout << codes[secUnits][i];
+			cout << " ";
+			cout << endl;
+		}
 	}
 
 	seconds--;
 	Sleep(1000);
+	system("cls");
 	goto STARTOFTIMER;
+
+
+ENDOFTIMER:
+	for (int i = 0; i < ROWS; i++)
+	{
+		cout << codes[0][i];
+		cout << " ";
+		cout << codes[0][i];
+		cout << " ";
+		if (i == 4 || i == 7) cout << char(178);
+		else cout << " ";
+		cout << " ";
+		cout << codes[0][i];
+		cout << " ";
+		cout << codes[0][i];
+		cout << " ";
+		cout << endl;
+	}
+	PlaySound(TEXT("alarmsound.wav") , NULL, SND_SYNC);
+
 }
